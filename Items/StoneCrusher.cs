f30e01afd147ef0c2ab21d26fa421d02189cecce
Rocketlauncher269 <84031530+Rocketlauncher269@users.noreply.github.com>
2022-10-01 22:21:02 +0100

    Update StoneCrusher.cs
    Stone crusher stats changed

--- Items/StoneCrusher.cs
+++ Items/StoneCrusher.cs
@@ -8,31 +8,31 @@ namespace AuroraMod.Items
 	{
 		public override void SetStaticDefaults()
 		{
-			// DisplayName.SetDefault("StoneCrusher"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
-			Tooltip.SetDefault("This is a basic modded sword.");
+			DisplayName.SetDefault("Stone Crusher");
+			Tooltip.SetDefault("");
 		}
 
 		public override void SetDefaults()
 		{
-			Item.damage = 50;
+			Item.damage = 12;
 			Item.DamageType = DamageClass.Melee;
-			Item.width = 40;
+			Item.width = 40; // will edit width and height later btw - rocket
 			Item.height = 40;
-			Item.useTime = 20;
-			Item.useAnimation = 20;
+			Item.useTime = 27;
+			Item.useAnimation = 27;
 			Item.useStyle = 1;
-			Item.knockBack = 6;
-			Item.value = 10000;
+			Item.knockBack = 7;
+			Item.value = 500;
 			Item.rare = 2;
 			Item.UseSound = SoundID.Item1;
-			Item.autoReuse = true;
+			Item.autoReuse = false;
 		}
 
 		public override void AddRecipes()
 		{
 			Recipe recipe = CreateRecipe();
-			recipe.AddIngredient(ItemID.DirtBlock, 10);
-			recipe.AddTile(TileID.WorkBenches);
+			recipe.AddIngredient(ItemID.StoneBlock, 25);
+			recipe.AddTile(TileID.Anvils);
 			recipe.Register();
 		}
 	}
