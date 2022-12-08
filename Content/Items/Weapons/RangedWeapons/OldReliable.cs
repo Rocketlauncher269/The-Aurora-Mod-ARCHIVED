f6bc8f59605e64567c52b8d44ff8cd9164b91b95
sucss <@sucss>
2022-12-08 11:20:35 +0100

    CrossbowItem (¬‿¬)

---
+++ Content/Items/Weapons/RangedWeapons/OldReliable.cs
@@ -0,0 +1,22 @@
+﻿using AuroraMod.Common;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AuroraMod.Content.Items.Weapons.RangedWeapons
+{
+    public class OldReliable : CrossbowItem
+    {
+        public override int UseTime => 75;
+
+        public override int Damage => 304;
+
+        public override float ShootSpeed => 14;
+        public override Vector2 DrawOriginOffset => new Vector2(20, -7);
+        public override Vector2 CenterOffset => Vector2.UnitY * 10;
+    }
+}
